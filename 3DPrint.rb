ca7cf1783750b0c1aa774ec056e89083c7c6820e
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2016-05-02 15:34:49 -0600

    Update 3DPrint.rb

--- 3DPrint.rb
+++ 3DPrint.rb
@@ -28,128 +28,128 @@ level9 =0.0
 #definitions PLAY WITH THEM buffer 2 
 ################################ 
 define :p1 do 
-with_fx :level, amp: level1 do 
-with_fx :slicer, phase: 0.1, wave: 1, mix: 0 do 
-sample P1, attack: 0, sustain: 3, rate: 0.2 
-end 
-sleep sample_duration P1 
-end 
+	with_fx :level, amp: level1 do 
+		with_fx :slicer, phase: 0.1, wave: 1, mix: 0 do 
+			sample P1, attack: 0, sustain: 3, rate: 0.2 
+		end 
+		sleep sample_duration P1 
+	end 
 end 
 
 define :p2 do 
-with_fx :level, amp: level2 do 
-with_fx :echo, amp: 0.2 do 
-sample P2, rate: 1 
-sleep sample_duration (P1) 
-end 
-end 
+	with_fx :level, amp: level2 do 
+		with_fx :echo, amp: 0.2 do 
+			sample P2, rate: 1 
+			sleep sample_duration (P1) 
+		end 
+	end 
 end 
 
 define :p3 do 
-with_fx :level, amp: level3 do 
-sample P3, rate: 1 
-sleep sample_duration (P1) 
-end 
+	with_fx :level, amp: level3 do 
+		sample P3, rate: 1 
+		sleep sample_duration (P1) 
+	end 
 end 
 
 define :p4 do 
-with_fx :level, amp: level4 do 
-sample P4, rate: 1 
-sleep sample_duration (P1) 
-end 
+	with_fx :level, amp: level4 do 
+		sample P4, rate: 1 
+		sleep sample_duration (P1) 
+	end 
 end 
 
 define :p5 do 
-with_fx :level, amp: level5 do 
-sample P5, rate: 1 
-sleep sample_duration (P1) 
-end 
+	with_fx :level, amp: level5 do 
+		sample P5, rate: 1 
+		sleep sample_duration (P1) 
+	end 
 end 
 
 
 define :p6 do 
-with_fx :level, amp: level6 do 
-sample P6, rate: 1 
-sleep sample_duration (P1) 
-end 
+	with_fx :level, amp: level6 do 
+		sample P6, rate: 1 
+		sleep sample_duration (P1) 
+	end 
 end 
 
 define :p7 do 
-with_fx :level, amp: level7 do 
-sample P7, rate: 1 
-sleep sample_duration (P1) 
-end 
+	with_fx :level, amp: level7 do 
+		sample P7, rate: 1 
+		sleep sample_duration (P1) 
+	end 
 end 
 
 define :p8 do 
-with_fx :echo, mix: 1 do 
-with_fx :level, amp: level8 do 
-sample P8, rate: 0.2 
-sleep sample_duration (P1) 
-end 
-end 
+	with_fx :echo, mix: 1 do 
+		with_fx :level, amp: level8 do 
+			sample P8, rate: 0.2 
+			sleep sample_duration (P1) 
+		end 
+	end 
 end 
 
 define :p9 do 
-with_fx :level, amp: level9 do 
-sample P9, rate: 1 
-sleep sample_duration (P1) 
-end 
+	with_fx :level, amp: level9 do 
+		sample P9, rate: 1 
+		sleep sample_duration (P1) 
+	end 
 end 
 
 ######################### 
 #Run the definitions BUFFER0 
 ######################### 
 in_thread do 
-loop do 
-p1 
-end 
-end 
-
-in_thread do 
-loop do 
-p2 
-end 
+	loop do 
+		p1 
+	end 
 end 
 
 in_thread do 
-loop do 
-p3 
-end 
-end 
-
-in_thread do 
-loop do 
-p4 
-end 
+	loop do 
+		p2 
+	end 
 end 
 
 in_thread do 
-loop do 
-p5 
-end 
-end 
-
-in_thread do 
-loop do 
-p6 
-end 
+	loop do 
+		p3 
+	end 
 end 
 
 in_thread do 
-loop do 
-p7 
-end 
-end 
-
-in_thread do 
-loop do 
-p8 
-end 
+	loop do 
+		p4 
+	end 
 end 
 
 in_thread do 
-loop do 
-p9 
+	loop do 
+		p5 
+	end 
 end 
+
+in_thread do 
+	loop do 
+		p6 
+	end 
+end 
+
+in_thread do 
+	loop do 
+		p7 
+	end 
+end 
+
+in_thread do 
+	loop do 
+		p8 
+	end 
+end 
+
+in_thread do 
+	loop do 
+		p9 
+	end 
 end
